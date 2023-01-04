c12c027729617c5a1f834dcb32b66499e97f4b40
sewon <kimsewon0076@gmail.com>
2023-01-04 18:01:06 +0900

    mainPage

--- public/js/about.js
+++ public/js/about.js
@@ -1,11 +1,13 @@
 // 스크롤하면 scrollBtn실행
 window.addEventListener("scroll", (event) => {
-    scrollBtn();
+    // scrollBtn();
     spacescroll();
 });
 function spacescroll() {
     let spaceship = document.querySelector("#spaceship");
     let sec1 = document.querySelector(".sec1");
+    let sec2 = document.querySelector(".sec2");
+    let mainPage = document.querySelector(".mainPage_wrap");
     let spaceUp = -300 + window.pageYOffset;
     if (document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10) {
         // console.log(window.pageYOffset);
@@ -20,8 +22,12 @@ function spacescroll() {
     // let sec1H = sec1.getBoundingClientRect;
     console.log(window.pageYOffset);
     if (window.pageYOffset >= 100 && window.pageYOffset < 1000) {
-        sec1.classList.add("fixed");
+        // sec1.classList.add("fixed");
+        mainPage.classList.add("fixed");
+        // sec2.classList.add("fixed");
+        // sec2.style.top = sec1.innerHeight;
     } else {
         sec1.classList.remove("fixed");
+        mainPage.classList.remove("fixed");
     }
 }
---
+++ public/js/mainPage_swiper.js
@@ -0,0 +1,12 @@
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>;
+var swiper = new Swiper(".sec3_swiper", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 25,
+    mousewheel: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: false,
+    },
+});
