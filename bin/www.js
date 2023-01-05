e8203e823ce21bb71ab9fa3ce07d5e8bffae00e1
sewon0076 <119268025+sewon0076@users.noreply.github.com>
2023-01-05 13:41:26 +0900

    horizontal swiper

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,7 @@
 //실행시켜주는 애
 let app = require("../app");
 let hostname = "127.0.0.1";
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3306;
 app.listen(port, () => {
     console.log(`Example app listening on port http://${hostname}:${port}/`);
     console.log(`${port}로 express 실행`);
