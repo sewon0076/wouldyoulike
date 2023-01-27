75bded8ce7b30c4530a462135f9589fab6e73572
sewon <kimsewon0076@gmail.com>
2023-01-27 17:51:03 +0900

    change package, need check db

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "test.cqi8uofs7gi9.ap-northeast-1.rds.amazonaws.com",
     user: "root",
-    password: "1234",
-    database: "wouldyoulike",
+    password: "sewon0076",
+    database: "wouldyou",
     multipleStatements: true, //데이터 여러개 넣는것
 });
 
