f14b283cb8aabe9fcc04454056e03e2466434095
sewon0076 <119268025+sewon0076@users.noreply.github.com>
2023-01-27 16:40:40 +0900

    small

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,8 @@
 //실행시켜주는 애
-let APP = require("../app");
-let HOSTNAME = "127.0.0.1";
-let PORT = process.env.PORT || 3308;
-app.listen(PORT, () => {
-    console.log(`Example app listening on port http://${HOSTNAME}:${PORT}/`);
-    console.log(`${PORT}로 express 실행`);
+let app = require("../app");
+let hostname = "127.0.0.1";
+let port = process.env.PORT || 3308;
+app.listen(port, () => {
+    console.log(`Example app listening on port http://${hostname}:${port}/`);
+    console.log(`${port}로 express 실행`);
 });
